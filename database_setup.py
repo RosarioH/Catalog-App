cfee031ca7ee43f9a3de804ad3a04db68de8059c
Hector-Rosario <ctstylez@hotmail.com>
2019-08-26 21:20:55 -0400

    recommit

--- database_setup.py
+++ database_setup.py
@@ -23,14 +23,15 @@ class Categories(Base):
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
+
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
         return {
             'name': self.name,
             'id': self.id,
+            
         }
-
 
 class CatalogItem(Base):
     __tablename__ = 'catalog_item'
@@ -45,6 +46,8 @@ class CatalogItem(Base):
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
+
+
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
@@ -55,8 +58,7 @@ class CatalogItem(Base):
             'price': self.price
         }
 
-
 engine = create_engine('sqlite:///catalogwithusers.db',
-                       connect_args={'check_same_thread': False})
+connect_args={'check_same_thread': False})
 
 Base.metadata.create_all(engine)
